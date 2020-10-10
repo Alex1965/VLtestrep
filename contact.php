c558b4f46f02d1121989b2d1bc40c6514de3f6c2
Jaspers <NESLJSP@nittoeurope.com>
2020-10-10 21:14:53 +0200

    update

--- contact.php
+++ contact.php
@@ -1,33 +1,13 @@
 <?php
 /*
- *  CONFIGURE EVERYTHING HERE
+ * A.Jaspers 2020
  */
-
-// an email address that will be in the From field of the email.
-$from = 'fromtest@client.com';
-
-// an email address that will receive the email with the output of the form
-$sendTo = 'xpsion@gmail.com';
-
-// subject of the email
+$from = 'contact@verweyletselschade.com';
+$sendTo = 'info@verweyletselschade.com';
 $subject = 'New message from contact form';
-
-// form field names and their translations.
-// array variable name => Text to appear in the email
-$fields = array('name' => 'Name','phone' => 'Phone', 'email' => 'Email', 'message' => 'Message'); 
-
-// message that will be displayed when everything is OK :)
-$okMessage = 'Contact form successfully submitted. Thank you, I will get back to you soon!';
-
-// If something goes wrong, we will display this message.
-$errorMessage = 'There was an error while submitting the form. Please try again later';
-
-/*
- *  LET'S DO THE SENDING
- */
-
+$fields = array('name' => 'Name','phone' => 'Phone', 'email' => 'Email','subject' => 'Subject','message' => 'Message'); 
 // if you are not debugging and don't need error reporting, turn this off by error_reporting(0);
-error_reporting(E_ALL & ~E_NOTICE);
+error_reporting(0);
 
 try
 {
@@ -37,39 +17,60 @@ try
     $emailText = "You have a new message from your contact form\n=============================\n";
 
     foreach ($_POST as $key => $value) {
-        // If the field exists in the $fields array, include it in the email 
         if (isset($fields[$key])) {
             $emailText .= "$fields[$key]: $value\n";
         }
+   
     }
 
-    // All the neccessary headers for the email.
+    //headers for the email.
     $headers = array('Content-Type: text/plain; charset="UTF-8";',
         'From: ' . $from,
         'Reply-To: ' . $from,
         'Return-Path: ' . $from,
     );
     
-    // Send email
+    // Send email to info@verweyletselschade.com
+    mail($sendTo, $subject, $emailText, implode("\n", $headers));
+        
+    //and to the sender
+    $sendTo = $_POST['email']; 
+    $from = 'info@verweyletselschade.com';
+    $subject = "confirmation of your request at VerweyLetselschade";
+    $emailText = "Thank you for contacting us!\r\nBelow is the information you provided.\r\n\rKind Regards,\r\nVerweyletselschade\r\n\r";
+    $emailText .= "provided information:\n\r"; 
+   
+    
+    foreach ($_POST as $key => $value) {
+        if (isset($fields[$key])) {
+            $emailText .= "$fields[$key]: $value\n";
+        }
+    }
+    $headers = array('Content-Type: text/plain; charset="UTF-8";',
+        'From: ' . $from,
+        'Reply-To: ' . $from,
+        'Return-Path: ' . $from,
+    );
     mail($sendTo, $subject, $emailText, implode("\n", $headers));
 
+    
+
+
     $responseArray = array('type' => 'success', 'message' => $okMessage);
+
+
 }
 catch (\Exception $e)
 {
     $responseArray = array('type' => 'danger', 'message' => $errorMessage);
 }
 
+if ($responseArray['type'] == 'success') {
+    // success redirect
 
-// if requested by AJAX request return JSON response
-if (!empty($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest') {
-    $encoded = json_encode($responseArray);
-
-    header('Content-Type: application/json');
-
-    echo $encoded;
+    header('Location: http://evaldesite.verweyletselschade.com/success.html');
 }
-// else just display the message
 else {
-    echo $responseArray['message'];
+    //error redirect
+    header('Location: http://evaldesite.verweyletselschade.com/error.html');
 }
